8fcc1e85712419be5389c06e0709c2d3057318e7
Abin M K <abinmkabin@gmail.com>
2025-04-07 21:53:25 +0530

    removed admin,kitchen components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Auth from './Pages/Auth/Auth';
 import WaiterDashboard from './Pages/Waiter/WaiterDashboard';
-// import KitchebDashboard from './Pages/Kitchen/KitchenDashboard';
-// import BillingDashboard from './Pages/Billing/BillingDashboard';
-// import AdminDashboard from './Pages/Admin/AdminDashboard';
 
 function App() {
   console.log("react app")
   return (
     <Routes>
       <Route path="/login" element={<Auth />} />
-      {/* <Route path="/admin" element={<AdminDashboard/>} /> */}
       <Route path="/waiter" element={<WaiterDashboard/>} />
-      {/* <Route path="/kitchen" element={<KitchebDashboard/>} />
-      <Route path="/cashier" element={<BillingDashboard/>} /> */}
     </Routes>
   );
 }
