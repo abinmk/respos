4e5651698c8b6c29c60fc26560d54a4c522ae91b
Abin M K <abinmkabin@gmail.com>
2025-05-10 13:29:17 +0530

    Added details of table images

--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -6,12 +6,12 @@ export default function Items({ selectedTable, setSelectedTable }) {
   const [quantities, setQuantities] = useState({});
 
   const items = [
-    { id: 1, name: "Chicken Biryani", price: 150, image: "https://source.unsplash.com/100x100/?biryani" },
-    { id: 2, name: "Margherita Pizza", price: 200, image: "https://source.unsplash.com/100x100/?pizza" },
-    { id: 3, name: "Veg Fried Rice", price: 120, image: "https://source.unsplash.com/100x100/?fried-rice" },
-    { id: 4, name: "Paneer Butter Masala", price: 180, image: "https://source.unsplash.com/100x100/?paneer" },
-    { id: 5, name: "Chicken 65", price: 140, image: "https://source.unsplash.com/100x100/?chicken" },
-    { id: 6, name: "Masala Dosa", price: 100, image: "https://source.unsplash.com/100x100/?dosa" },
+    { id: 1, name: "Chicken Biryani", price: 150, image: "https://media.istockphoto.com/id/2077426665/photo/selective-focus-of-one-of-the-most-popular-food-in-india-biryani.jpg?s=1024x1024&w=is&k=20&c=TrplV8CyNlyWQf0TU9yYqavs81Oq3GkCmuiVGVbgDGY=" },
+    { id: 2, name: "Margherita Pizza", price: 200, image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=2881&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { id: 3, name: "Veg Fried Rice", price: 120, image: "https://media.istockphoto.com/id/2180205841/photo/stir-fried-rice-with-mushroom-and-tofu-directly-above-photo.jpg?s=1024x1024&w=is&k=20&c=MP_zUAtEZ8WvoWIhKVlq9MndiV2eaNSfPErROQeHeCI=" },
+    { id: 4, name: "Paneer Butter Masala", price: 180, image: "https://media.istockphoto.com/id/1085159910/photo/malai-or-achari-paneer-in-a-gravy-made-using-red-gravy-and-green-capsicum-served-in-a-bowl.jpg?s=1024x1024&w=is&k=20&c=x-AgV0vfukGAAXVdbKVYwiI5b3a212D66gLSMk9g_cw=" },
+    { id: 5, name: "Chicken 65", price: 140, image: "https://media.istockphoto.com/id/1322439549/photo/indian-chicken-fry.jpg?s=1024x1024&w=is&k=20&c=kLLH1jaWiXDYeimYL4MAMpV3GLDLNI9GF4qoQ3ogPo4=" },
+    { id: 6, name: "Masala Dosa", price: 100, image: "https://images.unsplash.com/photo-1743517894265-c86ab035adef?q=80&w=2882&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   ];
 
   useEffect(() => {
@@ -96,6 +96,8 @@ export default function Items({ selectedTable, setSelectedTable }) {
         ...prev,
         [item.id]: result.item?.quantity ?? 0,
       }));
+
+      setSelectedTable({tableNumber:selectedTable.tableNumber});
     } catch (err) {
       toast.error("Server error. Try again later.");
     }
